e4c6123a7f7ebbf6f4494552b36d58d20e686485
Adrian Gierakowski <agierakowski@gmail.com>
2025-10-22 19:27:29 +0200

    add example and template

--- flake.nix
+++ flake.nix
@@ -2,5 +2,13 @@
   description = "Define collections of executable scripts with minimal boilerplate.";
   outputs = _: {
     flakeModule = ./nix/flake-module.nix;
+
+    templates.default = {
+      description = "Example flake using nix-scripts-module";
+      path = builtins.path {
+        path = ./example;
+        filter = path: _: baseNameOf path == "flake.nix";
+      };
+    };
   };
 }
